25242a56da2062b573fd6718f86f2465ddacd6f2
CreatorLynx <1_linx_1@mail.ru>
2026-07-30 08:03:08 +0300

    start work with torch interaction

--- Balors-tale-unity/Assets/Scenes/Game/torch/TorchController.cs
+++ Balors-tale-unity/Assets/Scenes/Game/torch/TorchController.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TorchController : MonoBehaviour
 {
-    
+    InputAction fireAction;
+
+    [SerializeField] AudioClip matchClip;
+    [SerializeField] AudioSource matchAudioSource;
+    [SerializeField] AudioSource burnupAudiosource;
+
+    Animator torchAnimator;
+
+    void Awake()
+    {
+        fireAction = InputSystem.actions.FindAction("Interact");
+    }
+    int countToFire = 3;
     void Update()
     {
         
+    }
+
+    void Matching()
+    {
+        matchAudioSource.PlayOneShot(matchClip);
+    }
+
+    void BurnUp()
+    {
+        torchAnimator.SetBool("IsLight", true);
+        burnupAudiosource.Play();
     }
 }
