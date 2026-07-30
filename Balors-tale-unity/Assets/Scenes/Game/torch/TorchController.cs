f7aa96b636564e63b4d8f0298443e6ce5b9db9ff
CreatorLynx <1_linx_1@mail.ru>
2026-07-30 08:55:42 +0300

    torch gameplay working

--- Balors-tale-unity/Assets/Scenes/Game/torch/TorchController.cs
+++ Balors-tale-unity/Assets/Scenes/Game/torch/TorchController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,9 @@ public class TorchController : MonoBehaviour
     [SerializeField] AudioSource matchAudioSource;
     [SerializeField] AudioSource burnupAudiosource;
 
+    [SerializeField] float baseBurningTime = 10f;
+    [SerializeField] float randomBurningTime = 5f;
+
     Animator torchAnimator;
 
     void Awake()
@@ -16,19 +20,40 @@ public class TorchController : MonoBehaviour
         fireAction = InputSystem.actions.FindAction("Interact");
     }
     int countToFire = 3;
+    bool isFireUp = false;
     void Update()
     {
-        
+        if(!isFireUp)
+        if(fireAction.WasPressedThisFrame())
+        {
+            Matching();
+        }
+
     }
 
+    IEnumerator BurnTime()
+    {
+        yield return new WaitForSeconds (baseBurningTime + Random.Range(0f, randomBurningTime));
+        isFireUp = false;
+        torchAnimator.SetBool("IsLight", false);
+        //sound burn down
+        //ambient off
+    }
+
+    int count = 0;
     void Matching()
     {
         matchAudioSource.PlayOneShot(matchClip);
+        //vfx
+        count++;
+        
     }
 
     void BurnUp()
     {
+        count = 0;
         torchAnimator.SetBool("IsLight", true);
         burnupAudiosource.Play();
+        StartCoroutine(BurnTime());
     }
 }
